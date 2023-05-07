e1e5a60b6bf0c8da42783a5dffcbbc571d804ad4
Diomedes Diaz <d.romeldiaz@gmail.com>
2023-05-07 00:18:40 -0500

    first commit

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import HeadBar from '@/components/HeadBar'
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <HeadBar />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
